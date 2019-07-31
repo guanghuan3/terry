fd7e779afcf699352b99b63a67e2bb7583b91cad
zhangwenbin <1411737783@qq.com>
2019-07-31 10:01:06 +0800

    [提交人员]：zhangwenbin [提交项目]：通用工具组件 [提交描述]：添加根据类型获取spring上下文中所有该类型的实例列表

--- archer/archer-utils-spring-boot-starter/src/main/java/com/terry/archer/utils/ApplicationContextUtil.java
+++ archer/archer-utils-spring-boot-starter/src/main/java/com/terry/archer/utils/ApplicationContextUtil.java
@@ -5,6 +5,8 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.Map;
+
 /**
  * Created by Administrator
  * on 2019/7/28.
@@ -12,16 +14,16 @@ import org.springframework.context.ApplicationContextAware;
 @Slf4j
 public class ApplicationContextUtil implements ApplicationContextAware {
 
-    private static ApplicationContext applicationContext;
+    private static ApplicationContext APPLICATION_CONTEXT;
 
     /**
-     * 获取spring上下文中的bean
+     * 获取spring上下文中的bean，根据beanId获取
      * @param beanId
      * @return
      */
     public static Object getBean(String beanId) {
         try {
-            return applicationContext.getBean(beanId);
+            return APPLICATION_CONTEXT.getBean(beanId);
         } catch (Exception e) {
             log.info("获取Bean失败：没有获取到[{}]", new Object[]{beanId});
             return null;
@@ -29,21 +31,41 @@ public class ApplicationContextUtil implements ApplicationContextAware {
     }
 
     /**
-     * 获取spring上下文中的bean
+     * 获取spring上下文中的bean，根据类型获取
      * @param clazz
      * @return
      */
     public static Object getBean(Class<?> clazz) {
         try {
-            return applicationContext.getBean(clazz);
+            return APPLICATION_CONTEXT.getBean(clazz);
         } catch (Exception e) {
             log.info("获取Bean失败：没有获取到[{}]", new Object[]{clazz});
             return null;
         }
     }
 
+    /**
+     * 根据类型获取该类型下的所有相关实例列表
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> Map<String, T> getBeans(Class<T> clazz) {
+        try {
+            return APPLICATION_CONTEXT.getBeansOfType(clazz);
+        } catch (Exception e) {
+            log.info("获取Bean列表失败：没有获取到相关[{}]类型的实例", new Object[]{clazz});
+            return null;
+        }
+    }
+
+    /**
+     * spring托管后自动装配context，赋值给静态属性供后续操作
+     * @param applicationContext
+     * @throws BeansException
+     */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        ApplicationContextUtil.applicationContext = applicationContext;
+        ApplicationContextUtil.APPLICATION_CONTEXT = applicationContext;
     }
 }
