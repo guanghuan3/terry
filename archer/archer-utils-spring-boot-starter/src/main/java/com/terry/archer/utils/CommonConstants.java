5c3b218580014e739b2be157d39369ffd7b2b530
Terry <guanghuan3@163.com>
2019-08-01 23:08:25 +0800

    [提交项目]：archer-excel和archer-common-util [提交人员]：zhangwenbin [提交描述]：提供开箱即用的基于springboot的excel导入导出工具包和通用工具包

---
+++ archer/archer-utils-spring-boot-starter/src/main/java/com/terry/archer/utils/CommonConstants.java
@@ -0,0 +1,14 @@
+package com.terry.archer.utils;
+
+/**
+ * Created by Administrator
+ * on 2019/8/1.
+ */
+public class CommonConstants {
+
+    public static final String NUMBER = "0123456789";
+
+    public static final String CAP_CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static final String LOW_CAHR = "abcdefghijklmnopqrstuvwxyz";
+}
