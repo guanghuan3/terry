c4c8701b30c98815ade215e8e5a11e648d9f2618
zhangwenbin <1411737783@qq.com>
2019-08-19 11:38:23 +0800

    [提交内容]：多数据源实现 [提交人员]：zhangwenbin [提交描述]：配置化实现多数据源切换工具包

--- archer/archer-datasource-spring-boot-starter/src/main/java/com/terry/archer/datasource/RoutingDataSource.java
+++ archer/archer-datasource-spring-boot-starter/src/main/java/com/terry/archer/datasource/RoutingDataSource.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.lang.Nullable;
 
 /**
- * 支持多数据库的路由数据源
+ * 支持主从多数据库的路由数据源
  */
 @Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
@@ -21,14 +21,25 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
         return DataSourceHolder.DATASOURCE_NAMES.get();
     }
 
-    public static void resetDatasource() {
+    public void resetDatasource() {
         log.info("Datasource is reset to default.");
         DataSourceHolder.DATASOURCE_NAMES.remove();
     }
 
-    public static void switchDatasource(String datasourceName) {
+    public void switchDatasource(String datasourceName) {
         log.info("Switch datasource to [{}]", new Object[]{datasourceName});
         DataSourceHolder.DATASOURCE_NAMES.set(datasourceName);
+    }
+
+    /**
+     * 切换默认的数据源，以此来控制事务关联的数据源
+     * @param targetDataSourceName
+     */
+    public void switchDefaultDataSource(String targetDataSourceName) {
+        // 支持事务，切换回默认的支持事务的数据源
+        log.info("Default datasource has been changed to [{}].", new Object[]{targetDataSourceName});
+        DataSourceHolder.DATASOURCE_NAMES.set(targetDataSourceName);
+        setDefaultTargetDataSource(determineTargetDataSource());
     }
 
     public String getDefaultDatasourceName() {
