14e4f3be5a43a8a41c8b36958c37d67a02f47aa8
zhangwenbin <1411737783@qq.com>
2019-08-19 11:39:48 +0800

    [提交内容]：多数据源实现 [提交人员]：zhangwenbin [提交描述]：配置化实现多数据源切换工具包

--- archer/archer-datasource-spring-boot-starter/src/main/java/com/terry/archer/datasource/annotation/DS.java
+++ archer/archer-datasource-spring-boot-starter/src/main/java/com/terry/archer/datasource/annotation/DS.java
@@ -14,4 +14,12 @@ public @interface DS {
      */
     String value();
 
+    /**
+     * 是否需要支持事务
+     * 默认为false不支持事务，事务绑定在默认的数据源
+     * 为true时，@Transactional将针对value关联的数据源进行事务控制
+     * @return
+     */
+//    boolean supportTransaction() default false;
+
 }
