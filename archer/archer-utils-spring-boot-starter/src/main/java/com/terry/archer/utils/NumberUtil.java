8af6f469aea1c58e688487ef94725c8593471a62
zhangwb <fast*123456>
2021-12-21 10:25:57 +0800

    [提交人]：guanghuan3 [提交内容]：添加maven安装本地jar到maven仓库的命令 [提交描述]：

--- archer/archer-utils-spring-boot-starter/src/main/java/com/terry/archer/utils/NumberUtil.java
+++ archer/archer-utils-spring-boot-starter/src/main/java/com/terry/archer/utils/NumberUtil.java
@@ -11,19 +11,35 @@ public class NumberUtil {
      * @return
      */
     public static String getRandNumStr(int len, int randRadio) {
+        return getRandNumStr(len, randRadio, true);
+    }
+
+    /**
+     * 获取补零后的随机数的字符串
+     * @param len       最大长度
+     * @param randRadio 随机数字取值范围
+     * @param fillZero  是否补零，true是，false否
+     * @return
+     */
+    public static String getRandNumStr(int len, int randRadio, boolean fillZero) {
         Random rand = new Random();
         String number = String.valueOf(rand.nextInt(randRadio));
 
-        if (number.length() < len) {
+        if (number.length() < len && fillZero) {
             StringBuffer sb = new StringBuffer(number);
             int scale = len - number.length();
             // 补零
             for (int i = 0; i < scale; i ++) {
                 sb.insert(0, 0);
             }
+            // 补零后的数字
             return sb.toString();
-        } else {
+        } else if (number.length() > len) {
+            // 超过长度后截取的数字
             return number.substring(0, len);
+        } else {
+            // 直接返回内容
+            return number;
         }
     }
 
